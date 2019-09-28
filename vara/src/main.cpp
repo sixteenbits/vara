97261d3ebf9febf0c3dc9c789187e667c84ca54a
crisconru <crisconru@gmail.com>
2019-09-28 15:24:10 +0200

    hola mundo

--- vara/src/main.cpp
+++ vara/src/main.cpp
@@ -2,8 +2,12 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hola mundo");
+  delay(3000);
+  
 }
